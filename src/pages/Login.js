c9037b9f646284f44e06961dd464b2705e39d96b
Rajendra Kulkarni <frankly.coder@gmail.com>
2021-10-18 23:07:37 +0530

    Implemented login UI

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,25 @@
 import React from "react";
 
+import styles from "../styles/login.module.css";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <form className={styles.loginForm}>
+      <span className={styles.loginSignupHeader}>Log In</span>
+
+      <div className={styles.field}>
+        <input type="email" placeholder="Email" required />
+      </div>
+
+      <div className={styles.field}>
+        <input type="password" placeholder="Paasword" required />
+      </div>
+
+      <div className={styles.field}>
+        <button>Log In</button>
+      </div>
+    </form>
+  );
 };
 
 export default Login;
